286e9725c7d701df4caa7edcf62ad5fcf65a98c7
SubhadeepZilong <63146468+SubhadeepZilong@users.noreply.github.com>
2022-04-25 10:24:39 +0530

    code

--- js/app.js
+++ js/app.js
@@ -5,4 +5,34 @@ dots:true,
 appendDots:'.slider-dots',
 dotsClass:'dots',
 });
+
+let hamburger = document.querySelector('.hamburger');
+let cross = document.querySelector('.cross');
+let mobileNav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', function(){
+    mobileNav.classList.add('open');
+});
+cross.addEventListener('click', function(){
+    mobileNav.classList.remove('open');
+});
+
+
+//Get the button:
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 });
